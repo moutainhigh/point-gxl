01fea04a106c28254a108afa0f0184a7a42665f3
wangxh <llwxg2@gmail.com>
2019-01-11 15:23:49 +0800

    1. 增加转换api

--- src/main/java/com/hds/xquark/dal/type/Trancd.java
+++ src/main/java/com/hds/xquark/dal/type/Trancd.java
@@ -47,8 +47,10 @@ public enum Trancd {
   CBM2("立省值2"),
   ACHA1("VIP首单推荐奖1"),
   ACHA2("VIP首单推荐奖2"),
-  SOCIAL_P("社区德分");
-
+  SOCIAL_P("社区德分"),
+  PACKET_POINT("德分红包"),
+  TRANSFER_C("德分转换"),
+  TRANSFER_P("转为收益");
   private final String desc;
 
   Trancd(String desc) {
